efb7b121212c44f67e82d8d7de6352b1bcae8a99
Daniel Juarez <hola@luisdanielhj.com>
2024-08-21 12:26:19 -0600

    now try

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,28 @@
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import './globals.css';
-import Head from 'next/head';
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Head from "next/head";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Daniel Hernandez',
-  description: '@luisdanielhj.com',
+  title: "Daniel Hernandez",
+  description: "@luisdanielhj.com",
   openGraph: {
     images: [
       {
-        url: '/daniel-hernandez.png',
+        url: "/daniel-hernandez.png",
         width: 1200,
         height: 630,
       },
     ],
   },
   twitter: {
-    images: ['/daniel-hernandez.png'],
+    images: ["/daniel-hernandez.png"],
   },
   icons: {
-    icon: '/daniel.png',
-    apple: '/daniel.png',
+    icon: "/daniel.png",
+    apple: "/daniel.png",
   },
 };
 
@@ -39,7 +39,12 @@ export default function RootLayout({
         <meta property="og:image" content="/daniel-hernandez.png" />
         <meta property="og:url" content="https://luisdanielhj.com" />
         <meta property="og:type" content="website" />
-        {/* Add any other global meta tags here */}
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta name="twitter:image" content="/daniel-hernandez.png" />
+        <meta name="twitter:image:type" content="website" />
+        <meta name="twitter:image:width" content="1200" />
+        <meta name="twitter:image:height" content="630" />
       </Head>
       <body className={inter.className}>{children}</body>
     </html>
