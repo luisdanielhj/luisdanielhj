51417c34d73ae7af1b186f6cbd94304c3140b6d1
Daniel Juarez <hola@luisdanielhj.com>
2024-08-21 12:13:47 -0600

    test opengraph

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,28 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Daniel Hernandez",
-  description: "@luisdanielhj.com",
+  title: 'Daniel Hernandez',
+  description: '@luisdanielhj.com',
+  openGraph: {
+    images: [
+      {
+        url: '/daniel-hernandez.png',
+        width: 1200,
+        height: 630,
+      },
+    ],
+  },
+  twitter: {
+    images: ['/daniel-hernandez.png'],
+  },
+  icons: {
+    icon: '/daniel.png',
+    apple: '/daniel.png',
+  },
 };
 
 export default function RootLayout({
